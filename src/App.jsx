8de56ed0cd504b5defd704f56bcef2e86ac73eba
syam suhith <421136@student.nitandhra.ac.in>
2025-11-06 21:19:04 +0530

    Delete App css file and modified the app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App(){
     return(
         <div>
-            <h1>I am Home page</h1>
+            <h1>I am Home page !!</h1>
+            <br />
+            <p>Welcome to git</p>
+            <br />
+            <br />
+            <div>
+                <a href="">click me</a>
+            </div>
         </div>
     )
 }
